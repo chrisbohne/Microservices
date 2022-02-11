5d9288259abe5366fd96372d120145ea8188e3dd
chrisbohne <christoph.bohne90@gmail.com>
2022-02-11 12:56:36 +0100

    Add query service

--- Mini_Microservices_App/comments/index.js
+++ Mini_Microservices_App/comments/index.js
@@ -38,7 +38,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('events', (req, res) => {
+app.post('/events', (req, res) => {
   console.log('Received Event', req.body.type);
 
   res.send({});
--- Mini_Microservices_App/event-bus/index.js
+++ Mini_Microservices_App/event-bus/index.js
@@ -8,11 +8,16 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
-
-  res.send({ status: ok });
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.error(err.message);
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.error(err.message);
+  });
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.error(err.message);
+  });
+  res.send({ status: 'Ok' });
 });
 
 app.listen(4005, () => {
